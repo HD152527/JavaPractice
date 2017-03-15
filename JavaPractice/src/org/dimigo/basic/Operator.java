6dacb668e60b5673306464f02c230a06b29bb53e
이승규 <seung4465@icloud.com>
2017-03-15 15:17:48 +0900

    asdf

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -5,7 +5,7 @@ package org.dimigo.basic;
  * org.dimigo.basic
  *   |_Operator
  *
- * 1. 개요 :
+ * 1. 개요 : 디미베네
  * 2. 작성일 : 2017. 3. 14.
  * <pre>
  *
@@ -20,7 +20,7 @@ public class Operator {
         int storeCount = 1500;
         long inGunBi = (long)salary * 12 * storeCount * employeePerStore;
 
-        System.out.println("<< 디미베네 연간 인건비");
+        System.out.println("<< 디미베네 연간 인건비 >>");
         System.out.println("월 평균 급여 : " + String.format("%,d",salary) + "원");
         System.out.println("점포 내 직원 수 : " + employeePerStore +"명");
         System.out.println("점포 수 : "+String.format("%,d",storeCount)+"개");
