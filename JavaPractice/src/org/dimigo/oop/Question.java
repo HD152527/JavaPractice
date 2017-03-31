a3ffcbc0f5cb46ff5717b14281314752c9b8bd4f
이승규 <seung4465@icloud.com>
2017-03-31 14:35:52 +0900

    jjj

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -16,26 +16,24 @@ import java.util.Scanner;
  */
 public class Question {
     public static void main(String args[]){
-        StringBuilder q1 = new StringBuilder("가장 좋아하는 가수는? ");
-        StringBuilder q2 = new StringBuilder("가장 좋아하는 배우는? ");
-        StringBuilder q3 = new StringBuilder("가장 좋아하는 과목은? ");
-        String answer1 = new String("빅뱅");
-        String answer2 = new String("원빈");
-        String answer3 = new String("응용 프로그래밍");
+
+
+        String question[] = {"가장 좋아하는 가수는? ", "가장 좋아하는 배우는? ","가장 좋아하는 과목은? "};
+        String answer[] = {"빅뱅", "원빈","응용 프로그래밍"};
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("1."+q1);
-        System.out.println(scan.nextLine().equals(answer1)?"정답입니다!":"틀렸습니다!");
-        System.out.println("2."+q2);
-        System.out.println(scan.nextLine().equals(answer2)?"정답입니다":"틀렸습니다!");
-        System.out.println("3."+q3);
-        System.out.println(scan.nextLine().equals(answer3)?"정답입니다!":"틀렸습니다!");
 
-        System.out.println("<< 결과 출력 >>");
-        System.out.println(q1.append(answer1 + "입니다").toString());
-        System.out.println(q2.append(answer2 + "입니다").toString());
-        System.out.println(q3.append(answer3 + "입니다").toString());
+        for(int i=0;i < 3; i++){
+            System.out.println((i+1) + "."+question[i]);
+            System.out.println(scan.nextLine().equals(answer[i])?"정답입니다!":"틀렸습니다!");
+        }
 
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.append("<< 결과 출력 >>"));
+        for(int i=0 ; i<3; i++){
+            sb.delete(0, 100);
+            System.out.println(sb.append(question[i] + answer[i] + "입니다").toString());
 
+        }
     }
 }
