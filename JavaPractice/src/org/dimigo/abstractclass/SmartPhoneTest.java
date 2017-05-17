964fd7964d26ffd578062dc704c210cecc475d16
이승규 <seung4465@icloud.com>
2017-05-17 15:26:01 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,32 @@
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_SmartPhoneTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 12.
+ * <pre>
+ *
+ * @author : Seung
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+    public static void main(String[] args){
+        SmartPhone[] phones = {
+                new IPhone("iPhone 7", "애플", 900000),
+                new Galaxy("갤럭시 S8", "삼성", 800000)
+
+        };
+        for (SmartPhone phone:phones
+             ) {
+            System.out.println(phone);
+            phone.turnOn();
+            phone.pay();
+            phone.useSpecialFunction();
+            phone.turnOff();
+            System.out.println();
+        }
+    }
+}
