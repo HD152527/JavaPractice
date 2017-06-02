90750944bcb4acd332d95d8c6d4bb8aec1d2b479
이승규 <seung4465@icloud.com>
2017-06-02 14:36:36 +0900

    시

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,43 @@
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *   |_Music
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 2.
+ * <pre>
+ *
+ * @author : Seung
+ * @version : 1.0
+ */
+public class Music {
+    private String title;
+    private String singer;
+
+    public Music(String title, String singer) {
+        this.title = title;
+        this.singer = singer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String toString(){
+        return title+" ("+singer+")";
+    }
+}
